19b9ab82757d029373d56064422cede2915154de
Shaivan19 <shaivan.jdgt@gmail.com>
2025-03-20 08:48:07 +0530

    Refactor MailUtil.js: remove commented-out code and streamline mail sending function

--- src/utils/MailUtil.js
+++ src/utils/MailUtil.js
@@ -1,36 +1,3 @@
-// //to,from,subject,text
-// const mailer = require('nodemailer');
-
-// ///function
-
-// const sendingMail = async(to,subject,text) => {
-
-//     const transporter = mailer.createTransport({
-//         service: 'gmail',
-//         auth:{
-//             user:"shaivan1909@gmail.com",
-
-//         }
-//     })
-
-//     const mailOptions = {
-//         from: 'shaivan1909@gmail.com',
-//         to: to,
-//         subject: subject,
-//         text: text
-//         //html:"<h1>"+text+"</h1>"
-//     }
-
-//     const mailresponse = await transporter.sendMail(mailOptions);
-//     console.log(mailresponse);
-//     return mailresponse;
-
-// }
-
-// module.exports ={
-//     sendingMail
-// }
-
 const nodemailer = require('nodemailer');
 
 const sendingMail = async (to, subject, text) => {
